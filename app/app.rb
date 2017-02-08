612bbacaac8268e04f8332546282d1dea7b74b13
Rob Holden <robholdenflag@gmail.com>
2017-02-08 16:27:37 +0000

    got test working

--- app/app.rb
+++ app/app.rb
@@ -34,7 +34,7 @@ end
     session[:user_id] = @user.id
     redirect to('/spaces')
     else
-      flash.next[:notice] = "Password and confirmation password do not match"
+      # flash.next[:notice] = "Password and confirmation password do not match"
       redirect ('/users/new')
     end
   end
@@ -45,11 +45,11 @@ end
 
 post '/sessions' do
   @user = User.authenticate(params[:email], params[:password])
-  if user
-    session[:user_id] = user.id
-    redirect to('/links')
+  if @user
+    session[:user_id] = @user.id
+    redirect to('/spaces')
   else
-    flash.now[:errors] = ['The email or password is incorrect']
+    # flash.now[:errors] = ['The email or password is incorrect']
     erb :'sessions/new'
   end
 end
--- spec/features/2_sign_in_spec.rb
+++ spec/features/2_sign_in_spec.rb
@@ -1,10 +1,13 @@
 feature 'User Sign In.' do
 
-  let!(:user) do
-   User.create(email: 'rob@example.com',
+  # let!(:user) do
+    user = User.create(email: 'rob@example.com',
                password: 'qwerty',
-               password_confirmation: 'qwerty')
-  end
+               password_confirmation: 'qwerty',
+               id: '1',
+               password_digest: '$2a$10$TnxHuGGbjJGGQ/OPT9EehOGG4FE.uzjNMISMJiIg/EANPhY0YUYZ2'
+               )
+  # end
 
   it 'authenticates when given a valid email address and password' do
     authenticated_user = User.authenticate(user.email, user.password)
